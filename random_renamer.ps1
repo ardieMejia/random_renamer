8b75278ca3432b5c095ac9daf30d234c518f49ca
Wan Ardie <wan_ahmad_ardie@yahoo.com>
2025-03-03 10:43:25 +0800

    fixed annoying site copied text

--- random_renamer.ps1
+++ random_renamer.ps1
@@ -5,6 +5,11 @@ $_input = Import-Csv -Path ./list.csv
 
 
 function Get-UniqueNewName{
+
+    param(
+        [Parameter(Position=0)]
+        [string]$l_ext
+    )
     $_used = Import-Csv -Path ./used.csv
     
     $num1 = Get-Random -Minimum 1 -Maximum $_input.Length
@@ -14,7 +19,7 @@ function Get-UniqueNewName{
     $adj = $_input[$num1].Adjectives
     $verb = $_input[$num1].Verbs
     $noun = $_input[$num3].Nouns
-    $new_name = "$($adj)_$($verb)_$($noun).png"
+    $new_name = "$($adj) $($verb) $($noun)$l_ext"
     
     while($new_name -In $_used."Used Names") {
 	# write-output "duplicate" # ========== NOTE-TO-SELF: functions in Powershell are weird, even Write-Output will be in return value, only use this when testing function
@@ -25,7 +30,7 @@ function Get-UniqueNewName{
 	$adj = $_input[$num1].Adjectives
 	$verb = $_input[$num1].Verbs
 	$noun = $_input[$num3].Nouns
-	$new_name = "$($adj)_$($verb)_$($noun).png"
+	$new_name = "$($adj) $($verb) $($noun)$l_ext"
     }
     return $new_name
 }
@@ -40,8 +45,8 @@ for($i=0; $i -lt $_listItems.Count; $i++){
     $length = $([String]$i).length
     $lefto = 5-$length
     $newname = "TEMP$("0"*$lefto)$i"
-    
-    Rename-Item $_listItems[$i].fullname "$($newname).png"    
+    $l_ext = $_listItems[$i].extension
+    Rename-Item $_listItems[$i].fullname "$($newname)$l_ext"    
 }
 
 
@@ -49,13 +54,13 @@ $listItems = Get-ChildItem -Path ./input
 
 
 for($i=0; $i -lt $_listItems.Count; $i++){
-    $new_name = Get-UniqueNewName
+    $new_name = Get-UniqueNewName($listItems[$i].extension)
     
     # ========== used.csv is for ensuring uniqueness
-    $used_name_object = @{
+    $used_name_object = [PSCustomObject]@{
 	"Used Names" = $new_name
     }    
-    $used_name_object | Export-CSV "used.csv" –Append
+    $used_name_object | Export-CSV "used.csv" -Append
     # ========== used.csv is for ensuring uniqueness    
     
     Write-Output "new name: $($new_name)"  
